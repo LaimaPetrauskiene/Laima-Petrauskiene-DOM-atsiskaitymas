c266be30e39ff1823a87c0d840a098ab8b158e9b
Laima Petrauskiene <laima@tinklai.net>
2021-11-15 18:50:35 +0200

    4: aprašyti request'ai į serverį

---
+++ js/API.js
@@ -0,0 +1,29 @@
+const baseURL = "http://localhost:3000";
+
+class API {
+    static fetchApartments = (success, failure) => {
+        fetch(`${baseURL}/apartment`)
+            .then(res => res.json())
+            .then(success)
+            .catch(failure)
+    }
+
+    static deleteApartment = (id, success, failure) => {
+        fetch(`${baseURL}/apartment/${id}`, { method: 'DELETE' })
+            .then(res => res.status === 200 ? success() : failure(res.statusText))
+            .catch(failure)
+    }
+
+}
+
+
+// API.fetchApartments(
+//    console.log,
+//    console.error
+// )
+
+// API.deleteApartment(
+//    "4",
+//    () => console.log('Ištrinta sėkmingai'),
+//    console.error
+// )
