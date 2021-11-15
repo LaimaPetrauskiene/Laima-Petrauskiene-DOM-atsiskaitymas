a9770aef33e1146326210456f43855a65e939e5b
Laima Petrauskiene <laima@tinklai.net>
2021-11-15 19:10:23 +0200

    6: duomenų siuntimas

--- js/components/ApartmentGridComponent.js
+++ js/components/ApartmentGridComponent.js
@@ -1,11 +1,48 @@
 class ApartmentGridComponent {
     constructor() {
+        this.state = {
+            loading: false,
+            apartments: []
+        }
         this.init();
     }
 
     init = () => {
+        this.state.loading = true;
+        this.initFetch();
         this.htmlElement = document.createElement('div');
-        this.htmlElement.innerHTML = 'Aš esu ApartmentGrid';
+
+        this.render();
     }
 
+    initFetch = () => {
+        API.fetchApartments(
+            (apartments) => {
+                this.state.loading = false;
+                this.saveApartments(apartments);
+            },
+            (err) => {
+                alert(err);
+                this.state.loading = false;
+                this.render();
+            }
+        );
+    }
+
+
+    saveApartments = (apartments) => {
+        this.state.apartments = apartments;
+        this.state.loading = false;
+
+        this.render();
+    }
+
+    render = () => {
+        const { loading } = this.state;
+        if (loading) {
+            this.htmlElement.innerHTML = `Siunčiama`;
+        } else {
+            this.htmlElement.innerHTML = `Parsiųsta!`;
+        }
+    }
 }
